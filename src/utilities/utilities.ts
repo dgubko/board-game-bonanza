b927b16bbf8dc00993516d26dc7f51915b84ca15
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-01-01 14:10:58 -0700

    Add typescript to clean top 100 function

--- src/utilities/utilities.ts
+++ src/utilities/utilities.ts
@@ -1,5 +1,15 @@
-export const cleanTop100Data = (data: any) => {
-    const cleanList = data.games.map((game: any) => {
+interface Game {
+    name: string;
+    price: string;
+    rank: number;
+    id: string;
+    thumb_url: string;
+    average_user_rating: number;
+    num_user_ratings: number;
+}
+
+export const cleanTop100Data = (data: {games: Game[]}) => {
+    const cleanList = data.games.map((game: Game) => {
         return {
             name: game.name,
             price: game.price,
