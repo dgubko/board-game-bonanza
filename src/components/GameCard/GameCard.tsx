3ae7cc39fa86b6a526ca4713d26b29ec48e629c9
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-01-03 17:27:53 -0700

    added basic css for readabilty

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,14 +1,15 @@
 import { CleanedGame } from "../../interfaces"
+import './GameCard.css'
 
 const GameCard = ({ name, price, rank, id, image, averageUserRating, numUserRatings }: CleanedGame) => {
     return (
-        <div>
-            <img src={image}/>
+        <div className="gameCard-container">
+            <p>Rank: #{rank}</p>
+            <img src={image} />
             <h3>{name}</h3>
             <p>${price}</p>
-            <p>{averageUserRating.toFixed(2)} / 5</p>
+            <p>{averageUserRating.toFixed(2)} / 5 ⭐️</p>
             <p>{numUserRatings} reviews</p>
-            <p>Rank: #{rank}</p>
         </div>
     )
 }
