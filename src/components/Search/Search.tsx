ace089e3d9310e2d4487ebf3fd0afa29fd0f607c
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-01-07 16:30:31 -0700

    Add search bar and functionality

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,26 @@
+import "./Search.css";
+
+type Props = {
+  query: string;
+  setQuery: (value: string) => void;
+};
+
+export const Search = (props: Props) => {
+  const handleInputChange = (event: any) => {
+    props.setQuery(event.target.value);
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className="input-form" onSubmit={handleSubmit}>
+      <input
+        value={props.query}
+        placeholder="Search by name"
+        onChange={handleInputChange}
+      />
+    </form>
+  );
+};
