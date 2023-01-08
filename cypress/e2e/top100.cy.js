2e4d76b80c55a40cefb333402ed71cc148810010
Courtney Lippman <courtney.lippman@colorado.edu>
2023-01-08 13:10:39 -0700

    Add tests for the top100 for the error messages.

--- cypress/e2e/top100.cy.js
+++ cypress/e2e/top100.cy.js
@@ -54,14 +54,124 @@ describe('Top 100', () => {
     cy.get('#fav-button').click()
     cy.get('.fav-game-card-wrapper').contains('Root')
   })
+
+  it('Should display top100 when top100 button is clicked from favorites', () => {
+    cy.get('.heart-container').eq(0).click()
+    cy.get('#fav-button').click()
+    cy.get('.fav-game-card-wrapper').contains('Root')
+
+    cy.get('#top100-button').click()
+    cy.get('.gameCard-container').should('have.length', 2)
+    cy.get('.gameCard-container').eq(0).should('contain', "Root")
+      .and('contain', '$48.00')
+      .and('contain', '4.06 / 5')
+      .and('contain', '404 reviews')
+      .within(() => {
+        cy.get('img').should('have.attr', 'src', "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1540147295104")
+        cy.get('button').eq(1).should('exist')
+      })
+
+    cy.get('.gameCard-container').eq(1).should('contain', "Scythe")
+      .and('contain', '$54.92')
+      .and('contain', '4.22 / 5')
+      .and('contain', '753 reviews')
+      .within(() => {
+        cy.get('img').should('have.attr', 'src', "https://cdn.shopify.com/s/files/1/0513/4077/1515/products/scythe-board-game.jpg?v=1611090922")
+        cy.get('button').eq(1).should('exist')
+      })
+    
+  })
   
   it('Displays details when game is clicked', () => {
     cy.intercept('https://api.boardgameatlas.com/api/search?ids=TAAifFP590&client_id=NO0Fq8pQcF', {
       method:"GET",
       fixture: "../fixtures/details.json"
     })
-    cy.get('.game-card-container').eq(0).click()
+    cy.get('.gameCard-container').eq(0).click()
     cy.get('.details-and-comment-container').should('exist')
   })
 
+  it('Navigate back to top100 page when clicking top100 button in details page', () => {
+    cy.intercept('https://api.boardgameatlas.com/api/search?ids=TAAifFP590&client_id=NO0Fq8pQcF', {
+      method:"GET",
+      fixture: "../fixtures/details.json"
+    })
+    cy.get('.gameCard-container').eq(0).click()
+    cy.get('.details-and-comment-container').should('exist')
+    cy.get('#top100-button').click()
+    cy.get('.gameCard-container').should('have.length', 2)
+    cy.get('.gameCard-container').eq(0).should('contain', "Root")
+      .and('contain', '$48.00')
+      .and('contain', '4.06 / 5')
+      .and('contain', '404 reviews')
+      .within(() => {
+        cy.get('img').should('have.attr', 'src', "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1540147295104")
+        cy.get('button').eq(1).should('exist')
+      })
+
+    cy.get('.gameCard-container').eq(1).should('contain', "Scythe")
+      .and('contain', '$54.92')
+      .and('contain', '4.22 / 5')
+      .and('contain', '753 reviews')
+      .within(() => {
+        cy.get('img').should('have.attr', 'src', "https://cdn.shopify.com/s/files/1/0513/4077/1515/products/scythe-board-game.jpg?v=1611090922")
+        cy.get('button').eq(1).should('exist')
+      })
+  })
+
+  it('Should be able to search for a specific game', () => {
+    cy.get('#search-input').type('Roo')
+    cy.get('.gameCard-container').eq(0).should('contain', "Root")
+    .and('contain', '$48.00')
+    .and('contain', '4.06 / 5')
+    .and('contain', '404 reviews')
+    .within(() => {
+      cy.get('img').should('have.attr', 'src', "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1540147295104")
+      cy.get('button').eq(1).should('exist')
+    })
+    cy.get('.gameCard-container').eq(1).should('not.exist')
+  })
+
+  it('Should show an error message when a user uses an incorrect url', () => {
+    cy.visit('http://localhost:3000/bananas')
+    cy.get('.page404').should('contain', '404')
+    .and('contain', 'Page not found')
+    .and('contain', "We're sorry, the page you requested could not be found. Please go back to the homepage.")
+    cy.get('#back-button').click()
+    cy.get('.gameCard-container').eq(0).should('contain', "Root")
+      .and('contain', '$48.00')
+      .and('contain', '4.06 / 5')
+      .and('contain', '404 reviews')
+      .within(() => {
+        cy.get('img').should('have.attr', 'src', "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1540147295104")
+        cy.get('button').eq(1).should('exist')
+      })
+
+    cy.get('.gameCard-container').eq(1).should('contain', "Scythe")
+      .and('contain', '$54.92')
+      .and('contain', '4.22 / 5')
+      .and('contain', '753 reviews')
+      .within(() => {
+        cy.get('img').should('have.attr', 'src', "https://cdn.shopify.com/s/files/1/0513/4077/1515/products/scythe-board-game.jpg?v=1611090922")
+        cy.get('button').eq(1).should('exist')
+      })
+
+  })
+
+
+  })
+
+
+  describe('Server-side error', () => {
+    beforeEach(() => {
+      cy.intercept('https://api.boardgameatlas.com/api/search?order_by=rank&ascending=false&limit=100&client_id=NO0Fq8pQcF', {
+        method:"GET"}, {
+          statusCode: 500,
+        })
+        cy.visit('http://localhost:3000/')
+    })
+    it('Should show response when there is a server error', () => {
+      cy.get('.error-modal').should('contain', 'Oops! Something went wrong!')
+      .and('contain', 'Please try again later')
+    })
   })
